e5c89e288f031851d5e3f282ff6e38528dbdb8ae
manojrammurthy <manrox.drag@gmail.com>
2022-08-21 16:49:38 +0530

    barely working app

---
+++ lib/main.dart
@@ -0,0 +1,438 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:record/record.dart';
+import 'package:path_provider/path_provider.dart';
+import 'audio_player.dart';
+import 'recorded_list_view.dart';
+
+void main() => runApp(const MyApp());
+
+class AudioRecorder extends StatefulWidget {
+  final void Function(String path) onStop;
+
+  const AudioRecorder({Key? key, required this.onStop}) : super(key: key);
+
+  @override
+  _AudioRecorderState createState() => _AudioRecorderState();
+}
+
+class _AudioRecorderState extends State<AudioRecorder> {
+  bool _isRecording = false;
+  bool _isPaused = false;
+  int _recordDuration = 0;
+  Timer? _timer;
+  Timer? _ampTimer;
+  final _audioRecorder = Record();
+  Amplitude? _amplitude;
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _ampTimer?.cancel();
+    _audioRecorder.dispose();
+    super.dispose();
+  }
+
+  Future<void> _start() async {
+    try {
+      if (await _audioRecorder.hasPermission()) {
+        // We don't do anything with this but printing
+        final isSupported = await _audioRecorder.isEncoderSupported(
+          AudioEncoder.aacLc,
+        );
+        if (kDebugMode) {
+          print('${AudioEncoder.aacLc.name} supported: $isSupported');
+        }
+      Directory appDirectory = await getApplicationDocumentsDirectory();
+      String filePath = appDirectory.path +
+        '/' +
+        DateTime.now().millisecondsSinceEpoch.toString() +
+        '.aacLc';
+        // final devs = await _audioRecorder.listInputDevices();
+
+        await _audioRecorder.start(path: '$filePath');
+
+        bool isRecording = await _audioRecorder.isRecording();
+        setState(() {
+          _isRecording = isRecording;
+          _recordDuration = 0;
+        });
+
+        _startTimer();
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> _stop() async {
+    _timer?.cancel();
+    _ampTimer?.cancel();
+    final path = await _audioRecorder.stop();
+
+    widget.onStop(path!);
+
+    setState(() => _isRecording = false);
+  }
+
+  Future<void> _pause() async {
+    _timer?.cancel();
+    _ampTimer?.cancel();
+    await _audioRecorder.pause();
+
+    setState(() => _isPaused = true);
+  }
+
+  Future<void> _resume() async {
+    _startTimer();
+    await _audioRecorder.resume();
+
+    setState(() => _isPaused = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildRecordStopControl(),
+                const SizedBox(width: 20),
+                _buildPauseResumeControl(),
+                const SizedBox(width: 20),
+                _buildText(),
+              ],
+            ),
+            if (_amplitude != null) ...[
+              const SizedBox(height: 40),
+              Text('Current: ${_amplitude?.current ?? 0.0}'),
+              Text('Max: ${_amplitude?.max ?? 0.0}'),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRecordStopControl() {
+    late Icon icon;
+    late Color color;
+
+    if (_isRecording || _isPaused) {
+      icon = const Icon(Icons.stop, color: Colors.red, size: 30);
+      color = Colors.red.withOpacity(0.1);
+    } else {
+      final theme = Theme.of(context);
+      icon = Icon(Icons.mic, color: theme.primaryColor, size: 30);
+      color = theme.primaryColor.withOpacity(0.1);
+    }
+
+    return ClipOval(
+      child: Material(
+        color: color,
+        child: InkWell(
+          child: SizedBox(width: 56, height: 56, child: icon),
+          onTap: () {
+            _isRecording ? _stop() : _start();
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPauseResumeControl() {
+    if (!_isRecording && !_isPaused) {
+      return const SizedBox.shrink();
+    }
+
+    late Icon icon;
+    late Color color;
+
+    if (!_isPaused) {
+      icon = const Icon(Icons.pause, color: Colors.red, size: 30);
+      color = Colors.red.withOpacity(0.1);
+    } else {
+      final theme = Theme.of(context);
+      icon = const Icon(Icons.play_arrow, color: Colors.red, size: 30);
+      color = theme.primaryColor.withOpacity(0.1);
+    }
+
+    return ClipOval(
+      child: Material(
+        color: color,
+        child: InkWell(
+          child: SizedBox(width: 56, height: 56, child: icon),
+          onTap: () {
+            _isPaused ? _resume() : _pause();
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildText() {
+    if (_isRecording || _isPaused) {
+      return _buildTimer();
+    }
+
+    return const Text("Waiting to record");
+  }
+
+  Widget _buildTimer() {
+    final String minutes = _formatNumber(_recordDuration ~/ 60);
+    final String seconds = _formatNumber(_recordDuration % 60);
+
+    return Text(
+      '$minutes : $seconds',
+      style: const TextStyle(color: Colors.red),
+    );
+  }
+
+  String _formatNumber(int number) {
+    String numberStr = number.toString();
+    if (number < 10) {
+      numberStr = '0' + numberStr;
+    }
+
+    return numberStr;
+  }
+
+  void _startTimer() {
+    _timer?.cancel();
+    _ampTimer?.cancel();
+
+    _timer = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() => _recordDuration++);
+    });
+
+    _ampTimer =
+        Timer.periodic(const Duration(milliseconds: 200), (Timer t) async {
+      _amplitude = await _audioRecorder.getAmplitude();
+      setState(() {});
+    });
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+   int currentIndex = 0;
+   final widgetTitle = ["Home", "Record Audio", "playfrompapad"];
+   late Directory appDirectory;
+  bool showPlayer = false;
+  String? audioPath;
+  List<String> records = [];
+
+  @override
+  void initState() {
+    showPlayer = false;
+    super.initState();
+     getApplicationDocumentsDirectory().then((value) {
+      appDirectory = value;
+      appDirectory.list().listen((onData) {
+        if (onData.path.contains('.aac')) records.add(onData.path);
+      }).onDone(() {
+        records = records.reversed.toList();
+        setState(() {});
+      });
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RadioHome',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title:  Text(widgetTitle.elementAt(currentIndex)),
+        ),
+        body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+           child: AudioRecorder(
+                  onStop: (path) {
+                    if (kDebugMode) print('Recorded file path: $path');
+                    setState(() {
+                      audioPath = path;
+                      print('$audioPath');
+                     // showPlayer = true;
+                    });
+                  },
+                ),
+          ),
+           Expanded(
+            flex: 2,
+            child: RecordListView(
+              records: records,
+            ),
+          ),
+        ],
+      ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed, // Fixed
+          backgroundColor: Colors.blue,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          items:  [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Record Audio',
+              icon: Icon(Icons.mic),
+            ),
+            BottomNavigationBarItem(
+              label: 'Play From Papad',
+              icon: Icon(Icons.radio),
+            )
+          ],
+          currentIndex: currentIndex,
+          onTap: (int Index) {
+            setState(
+              () {
+                currentIndex = Index;
+                switch (Index) {
+                  case 0:
+                    //Navigator.pushNamed(context, '/home');
+                    //print(context);
+                    break;
+                  case 1:
+                    //Navigator.pushNamed(context, '/recordAudio');
+                    print(Index);
+                    break;
+                  case 2:
+                    //Navigator.pushNamed(context,'/playfrompapad');
+                    print(Index);
+                    break;
+                  default:
+                    //Navigator.pushNamed(context, '/home');
+                    print(Index);
+                    break;
+                }
+                
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: showPlayer
+//               ? Padding(
+//                   padding: const EdgeInsets.symmetric(horizontal: 25),
+//                   child: AudioPlayer(
+//                     source: audioPath!,
+//                     onDelete: () {
+//                       setState(() => showPlayer = false);
+//                     },
+//                   ),
+//                 )
+//               : AudioRecorder(
+//                   onStop: (path) {
+//                     if (kDebugMode) print('Recorded file path: $path');
+//                     setState(() {
+//                       audioPath = path;
+//                       print('$audioPath');
+//                       showPlayer = true;
+//                     });
+//                   },
+//                 ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   String buttonName = 'click';
+//   int currentIndex = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('App Title'),
+//         ),
+//         body: Center(
+//           child: currentIndex == 0 ? Container(
+//             width: double.infinity,
+//             color: Colors.yellow,
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 ElevatedButton(
+//                   style: ElevatedButton.styleFrom(
+//                     onPrimary: Colors.white,
+//                     primary: Colors.green
+//                   ),
+//                   onPressed: () {
+//                     setState(() {
+//                       buttonName = 'clicked';
+//                     });
+//                   },
+//                   child: Text(buttonName),
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     setState(() {
+//                       buttonName = 'clicked';
+//                     });
+//                   },
+//                   child: Text(buttonName),
+//                 ),
+//               ],
+//             ),
+//           ): Image.asset('images/northernlights.jpg'),
+//         ),
+//         bottomNavigationBar: BottomNavigationBar(
+//           items: const [
+//             BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+//             BottomNavigationBarItem(
+//                 label: 'Settings', icon: Icon(Icons.settings))
+//           ],
+//           currentIndex: currentIndex,
+//           onTap: (int Index) {
+//             setState(() {
+//               currentIndex = Index;
+//             });
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
